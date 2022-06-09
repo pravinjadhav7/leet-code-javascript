a50867b7be7060d377c93e24eb37326faaadf30b
Pravin Jadhav <pravinjadhav762@gmail.com>
2022-06-09 12:08:27 +0530

    Create longest_substring.js

---
+++ longest_substring.js
@@ -0,0 +1,59 @@
+// Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    // Base condition
+    if (!s) {
+        return 0;
+    }
+    // Starting index of the window
+    let start = 0;
+    // Ending index of the window
+    let end = 0;
+    // Maximum length of the substring
+    let maxLength = 0;
+    // Set to store the unique characters
+    const uniqueCharacters = new Set();
+    // Loop for each character in the string
+    while (end < s.length) {
+        if (!uniqueCharacters.has(s[end])) {
+            uniqueCharacters.add(s[end]);
+            end++;
+            maxLength = Math.max(maxLength, uniqueCharacters.size);
+        } else {
+            uniqueCharacters.delete(s[start]);
+            start++;
+        }
+    }
+    return maxLength;
+};
