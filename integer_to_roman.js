1b0c42dfe870ee72eacd1216b1c2dfaf1598271e
Pravin Jadhav <pravinjadhav762@gmail.com>
2022-08-08 11:04:35 +0530

    Easy solution added

--- integer_to_roman.js
+++ integer_to_roman.js
@@ -45,6 +45,37 @@
  * @param {number} num
  * @return {string}
  */
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+//Solution 1
+
+const integers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];   
+    
+
+var intToRoman = function(num) {
+    let res = '';
+    let i = 0
+    while(num > 0) {
+        
+        if(num >= integers[i]) {
+            res += roman[i]
+            num -= integers[i];
+        }
+        
+        else
+            i++;
+    }
+    
+    return res;
+};
+
+
+// Solution 2
+
 var intToRoman = function(num) {
     
 const roman_digits = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX',
